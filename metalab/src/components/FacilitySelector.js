6a21e4a4d49ee45380e82b640a5f6083654a9988
Adarsh Patel <swe.adarsh@gmail.com>
2021-04-09 10:54:22 -0400

    finished facility selector

--- metalab/src/components/FacilitySelector.js
+++ metalab/src/components/FacilitySelector.js
@@ -1,34 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../Index.css";
 import departments from "../facilitiesInfo";
 
 const departmentTitles = ["Chemical", "Mechanical", "Metallurgical"];
 
-const FacilitySelector = (props) => {
+const FacilitySelector = ({ changeDept, changeTest }) => {
   const [dept, setDept] = useState("none");
   const [testName, setTestName] = useState("none");
+
+  useEffect(() => {
+    changeTest(testName);
+  }, [testName]);
+
+  useEffect(() => {
+    changeDept(dept);
+  }, [dept]);
 
   return (
     <div className="facilitiesSelector">
       {departmentTitles.map((name) => {
         return (
-          <div
-            className={name === dept ? "facilityBtnActive" : "facilityBtn"}
-            onClick={() => {
-              setDept(name);
-            }}
-          >
-            <p>{name}</p>
+          <div>
+            <div
+              className={name === dept ? "facilityBtnActive" : "facilityBtn"}
+              onClick={() => {
+                setDept(name);
+                setTestName("none");
+              }}
+            >
+              <p>{name}</p>
+            </div>
             <div>
               {dept !== "none" && dept === name
                 ? departments[dept].tests.map((test) => {
                     return (
                       <div
                         className={
-                          testName === test ? "testBtnActive" : "testBtn"
+                          testName === test.name ? "testBtnActive" : "testBtn"
                         }
                         onClick={() => {
-                          setTestName(test);
+                          setTestName(test.name);
                         }}
                       >
                         <p>{test.name}</p>
--- metalab/src/pages/facilities.js
+++ metalab/src/pages/facilities.js
@@ -3,9 +3,20 @@ import departments from "../facilitiesInfo";
 import FacilitySelector from "../components/FacilitySelector";
 import NavBar from "../components/NavBar";
 
-const facilities = () => {
+const FacilitiesPage = () => {
   const departmentList = ["Chemical", "Mechanical", "Metallurgical"];
   const test = "none";
+
+  const [currDept, setCurrDept] = useState("none");
+  const [currTest, setCurrTest] = useState("none");
+
+  const handleDeptChange = (dept) => {
+    setCurrDept(dept);
+  };
+
+  const handleTestChange = (test) => {
+    setCurrTest(test);
+  };
 
   return (
     <div>
@@ -13,11 +24,15 @@ const facilities = () => {
       <main className="facilitiesBody">
         <div className="facilitiesList">
           <h4>Select a Department:</h4>
-          <FacilitySelector />
+          <FacilitySelector
+            changeTest={handleTestChange}
+            changeDept={handleDeptChange}
+          />
         </div>
         <div className="facilitiesInfo">
           <div>
-            <h2> Chemical </h2>
+            <h2>{currDept} Department</h2>
+            <h2>{currTest}</h2>
             <p>{departments.Chemical.info}</p>
             <h3>Tests:</h3>
             <ul>
@@ -32,4 +47,4 @@ const facilities = () => {
   );
 };
 
-export default facilities;
+export default FacilitiesPage;
